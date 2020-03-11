52b5421810ae886b7ee86fa6082503c93432a96a
dyf901 <1378550520@qq.com>
2020-03-11 17:11:50 +0800

    2020/3/11 17:20    智慧工地云平台项目代码备份

---
+++ src/main/java/com/zty/smart_site/controller/DepartmentController.java
@@ -0,0 +1,17 @@
+package com.zty.smart_site.controller;
+
+import com.zty.smart_site.service.DepartmentService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(description = "部门接口")
+@RestController
+@RequestMapping("department")
+@CrossOrigin
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+}
---
+++ src/main/java/com/zty/smart_site/service/DepartmentService.java
@@ -0,0 +1,8 @@
+package com.zty.smart_site.service;
+
+import com.zty.smart_site.dao.DepartmentDao;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService implements DepartmentDao {
+}
