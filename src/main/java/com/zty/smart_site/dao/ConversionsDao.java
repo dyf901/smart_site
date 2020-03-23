c78a0f4707d43d95488accd041b1f272f2ba8470
dyf901 <1378550520@qq.com>
2020-03-23 19:45:07 +0800

    2020/3/23 19.44:00    智慧工地云平台项目代码备份

--- src/main/java/com/zty/smart_site/dao/ConversionsDao.java
+++ src/main/java/com/zty/smart_site/dao/ConversionsDao.java
@@ -1,7 +1,28 @@
 package com.zty.smart_site.dao;
 
+import com.zty.smart_site.entity.Conversions;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface ConversionsDao {
+    //增加兑换记录
+    int InsertConversions(Map map);
+
+    //删除兑换记录
+    int DeleteConversions(Map map);
+
+    //分页模糊查询兑换记录
+    List<Conversions> FindConversions(Map map);
+
+    //统计总数
+    long Total(Map map);
+
+    //分页模糊查询个人兑换记录
+    List<Conversions> FindConversionsByStaffId(Map map);
+
+    //统计个人总数
+    long TotalByStaff(Map map);
 }
--- src/main/java/com/zty/smart_site/service/ConversionsService.java
+++ src/main/java/com/zty/smart_site/service/ConversionsService.java
@@ -1,8 +1,51 @@
 package com.zty.smart_site.service;
 
 import com.zty.smart_site.dao.ConversionsDao;
+import com.zty.smart_site.entity.Conversions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ConversionsService implements ConversionsDao {
+    @Autowired
+    private ConversionsDao conversionsDao;//兑换记录
+
+    //增加兑换记录
+    @Override
+    public int InsertConversions(Map map) {
+        return conversionsDao.InsertConversions(map);
+    }
+
+    //删除兑换记录
+    @Override
+    public int DeleteConversions(Map map) {
+        return conversionsDao.DeleteConversions(map);
+    }
+
+    //分页模糊查询兑换记录
+    @Override
+    public List<Conversions> FindConversions(Map map) {
+        return conversionsDao.FindConversions(map);
+    }
+
+    //统计总数
+    @Override
+    public long Total(Map map) {
+        return conversionsDao.Total(map);
+    }
+
+    //分页模糊查询个人兑换记录
+    @Override
+    public List<Conversions> FindConversionsByStaffId(Map map) {
+        return conversionsDao.FindConversionsByStaffId(map);
+    }
+
+    //统计个人总数
+    @Override
+    public long TotalByStaff(Map map) {
+        return conversionsDao.TotalByStaff(map);
+    }
 }
